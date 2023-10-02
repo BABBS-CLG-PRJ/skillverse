afc708aec17d97c46c819a34053daed245de799a
arnab7070 <arnabnandi7074@gmail.com>
2023-10-02 10:52:25 +0530

    Updated apis.js file with all endpoints till now

--- app/services/apis.js
+++ app/services/apis.js
@@ -11,4 +11,27 @@ export const catalogData = {
 export const contactusEndpoint = {
   CONTACT_US_API: BASE_URL + "/contact/contactUs",
 };
-
+// USER-REGISTRATION API
+export const registerEndpoint = {
+  REGISTER_API: BASE_URL + "/api/adduser",
+}
+// GET OTP API
+export const otpEndpoint = {
+  OTP_API: BASE_URL + "/api/getotp",
+}
+// RESET PASSWORD API
+export const resetpasswordEndpoint = {
+  RESET_PASSWORD_API: BASE_URL + "/api/resetpassword",
+}
+// LOGIN API
+export const loginEndpoint = {
+  LOGIN_API: BASE_URL + "/api/validateuser",
+}
+// VERIFY OTP API
+export const verifyotpEndpoint = {
+  VERIFY_OTP_API: BASE_URL + "/api/verifyotp",
+}
+// VERIFY TOKEN API
+export const verifytokenEndpoint = {
+  VERIFY_TOKEN_API: BASE_URL + "/api/verifytoken",
+}
