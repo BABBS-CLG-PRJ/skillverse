78078de1784cca0d11748c5f633b21a835158fdd
Supra707 <supratikde2@gmail.com>
2023-10-16 18:30:28 +0530

    motamuti otp integration with sign up page done

--- app/SignUp/page.jsx
+++ app/SignUp/page.jsx
@@ -8,33 +8,18 @@ function Signup() {
   const[loading2,setLoading2]=useState(false);
   const[otp_sent2,setOtpsent2]=useState(false);
   function otp_loading2(otp_sent1,loading1){
-
       setLoading2(loading1);
       setOtpsent2(otp_sent1);
   }
-  
-  
-  // console.log("loading (determines the loading screen) for ultimate sign up:",loading2);
-  // console.log("Otp-sent (determines the verify otp page) for ultimate sign up:",otp_sent2);
   return (
-  //   loading2?( <div className=".custom-loader w-[100%] pt-[30%] pb-[30%]">
-  //   <div className="custom-loader"></div>
-  // </div>):(otp_sent2?(<Verify_otp/>):( ))
-  <div>
-    <div className="flex flex-col items-center">
-      <span>Loading:{loading2.toString()}</span>
-      <span>otp_sent:{otp_sent2.toString()}</span>
-      </div>
-  <Template
-  title="Join the millions upskilling with Skillverse"
-  description1="Build skills for today, tomorrow, and beyond."
-  description2="Education to future-proof your career."
-  image={signupImg}
-  formType="signup"
-  otp_loading2={otp_loading2}
-/>
-</div>
-  );
+   otp_sent2?(<Verify_otp/>):( <Template
+    title="Join the millions upskilling with Skillverse"
+    description1="Build skills for today, tomorrow, and beyond."
+    description2="Education to future-proof your career."
+    image={signupImg}
+    formType="signup"
+    otp_loading2={otp_loading2}
+  />));
 }
 
 export default Signup;
