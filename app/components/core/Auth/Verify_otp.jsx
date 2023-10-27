d8c4906b9ce79892cde4c032be8a21d0458902d2
Supra707 <supratikde2@gmail.com>
2023-10-27 18:09:32 +0530

    finally uff

--- app/components/core/Auth/Verify_otp.jsx
+++ app/components/core/Auth/Verify_otp.jsx
@@ -1,46 +1,46 @@
 import React, { useEffect, useState } from "react";
 import OTPInput from "react-otp-input";
-const Verify_otp = () => {
-const[otp,setOtp]=useState('');
-const handleSubmit=(e)=>{
-  e.preventDefault();
-  console.log(otp);
-  setOtp('');
-}
-  return (
-    <div>
-      <div className="min-h-[calc(100vh-3.5rem)] grid place-items-center px-4 md:px-0">
-        <div className="w-full md:max-w-[500px] p-4 lg:p-8">
-          <h1 className="text-[#652429] md:w-full w-1/2 font-extrabold text-center text-[1.875rem] leading-[2.375rem]">
-            Verify Email
-          </h1>
-          <p className="text-[1.125rem] w-1/2 md:w-full  leading-[1.625rem] my-4 mx-6 text-[#000000] font-semibold">
-            A verification code has been sent to you. Enter the code below
-          </p>
-          <form onSubmit={handleSubmit}>
-          <OTPInput
-                    value={otp}
-                    onChange={setOtp}
-                    numInputs={6}
-                    renderSeparator={<span>-</span>}
-                    inputStyle="rounded-[8px] border-[2px] border-[#652429] text-[3rem] text-center text-black w-10 md:w-14 lg:w-16"
-                    isInputNum={true}
-                    shouldAutoFocus={true}
-                    containerStyle="flex justify-evenly gap-1"
-                    renderInput={(props) => <input {...props}
-                    style={{ width: '1.5em' }}
-                  
-                 />}
 
-                    />
-            <button
-              type="submit"
-              className="md:w-full w-1/2 bg-primary-yellow py-[12px] px-[12px] rounded-[8px] mt-6 mx-4 hover:bg-yellow-300 transition-all duration-500 font-medium text-richblack-900"
-            >
-              Verify Email
-            </button>
-          </form>
-        </div>
+const Verify_otp = () => {
+  const [otp, setOtp] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(otp);
+    setOtp("");
+  };
+
+  return (
+    <div className="min-h-[calc(100vh-3.5rem)] grid place-items-center px-4 md:px-0">
+      <div className="w-full md:w-1/2 flex flex-col items-center p-4 lg:p-8">
+        <h1 className="text-[#652429] font-extrabold text-center text-4xl leading-10">
+          Verify Email
+        </h1>
+        <p className="text-lg leading-6 my-4 mx-6 text-[#000000] font-semibold">
+          A verification code has been sent to you. Enter the code below
+        </p>
+
+        <form>
+          <OTPInput
+            value={otp}
+            onChange={setOtp}
+            numInputs={6}
+            renderSeparator={<span></span>}
+            inputStyle="rounded-[8px] border-2 border-[#652429] text-3xl text-center text-black"
+            isInputNum={true}
+            shouldAutoFocus={true}
+            containerStyle="flex justify-evenly gap-1"
+            renderInput={(props) => (
+              <input {...props} style={{ width: "1.5em" }} />
+            )}
+          />
+        </form>
+        <button
+          onClick={handleSubmit}
+          type="submit"
+          className="w-full lg:w-1/2 rounded-md bg-primary-yellow py-3 px-3 rounded-8 mt-6 mx-4 hover:bg-yellow-300 transition-all duration-500 font-medium text-richblack-900"
+        >
+          Verify Email
+        </button>
       </div>
     </div>
   );
