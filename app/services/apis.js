14660fa44a8c39e36f6c3de0f227485b34a74dea
Supra707 <supratikde2@gmail.com>
2023-10-28 17:26:09 +0530

    change

--- app/services/apis.js
+++ app/services/apis.js
@@ -1,15 +1,7 @@
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-// RATINGS AND REVIEWS
-export const ratingsEndpoints = {
-  REVIEWS_DETAILS_API: BASE_URL + "/course/getReviews",
-};
-// CATALOG PAGE DATA
-export const catalogData = {
-  CATALOGPAGEDATA_API: BASE_URL + "/course/getCategoryPageDetails",
-};
 // CONTACT-US API
 export const contactusEndpoint = {
-  CONTACT_US_API: BASE_URL + "/contact/contactUs",
+  CONTACT_US_API: BASE_URL + "/api/contactUs",
 };
 // USER-REGISTRATION API
 export const registerEndpoint = {
