7a2cb75bc47787f9ed3640c8c4c8f85d0b8daa97
Bineet09 <bineetchattopadhyay00@gmail.com>
2024-03-17 21:48:18 +0530

    minor changes

--- app/courses/[CourseId]/page.jsx
+++ app/courses/[CourseId]/page.jsx
@@ -25,9 +25,9 @@ const CoursePage = ({ params }) => {
   return (
     <div>
       
-      <div className="h-[60vh] flex flex-col md:flex-row justify-between text-brown px-14 items-center">
+      <div className="h-2/3 flex flex-col md:flex-row justify-between text-brown px-14 mt-5 items-center">
             <div>
-                <h1 className="font-sans text-6xl font-bold mb-5">{courseData?.title}</h1>
+                <h1 className="font-sans text-4xl md:text-6xl font-bold mb-5">{courseData?.title}</h1>
                 {/* <p>{author}</p> */}
                 <p className='text-lg'>{courseData?.description}</p>
                 {/* <p>{courseData?.price}</p>   */}
@@ -57,7 +57,7 @@ const CoursePage = ({ params }) => {
 
 
       {/* this is Curriculum */}
-      <div className="w-[800px] px-14 mt-20">
+      <div className="w-3/4 px-14 mt-20">
         <h2 className='font-sans text-4xl font-semibold mb-3'>Curriculum</h2>
 
         {courseData ? (
@@ -83,53 +83,11 @@ const CoursePage = ({ params }) => {
               {/* <p class="text-gray-500 dark:text-gray-400">Check out this guide to learn how to <a href="/docs/getting-started/introduction/" class="text-blue-600 dark:text-blue-500 hover:underline">get started</a> and start developing websites even faster with components on top of Tailwind CSS.</p> */}
             </div>
           </div>
-
-          
-          {/* <h2 id="accordion-flush-heading-2">
-            <button type="button" class="flex items-center justify-between w-full py-5 font-medium rtl:text-right text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400 gap-3" data-accordion-target="#accordion-flush-body-2" aria-expanded="false" aria-controls="accordion-flush-body-2">
-              <span>Is there a Figma file available?</span>
-              <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
-              </svg>
-            </button>
-          </h2>
-          <div id="accordion-flush-body-2" class="hidden" aria-labelledby="accordion-flush-heading-2">
-            <div class="py-5 border-b border-gray-200 dark:border-gray-700">
-              <p class="mb-2 text-gray-500 dark:text-gray-400">Flowbite is first conceptualized and designed using the Figma software so everything you see in the library has a design equivalent in our Figma file.</p>
-              <p class="text-gray-500 dark:text-gray-400">Check out the <a href="https://flowbite.com/figma/" class="text-blue-600 dark:text-blue-500 hover:underline">Figma design system</a> based on the utility classes from Tailwind CSS and components from Flowbite.</p>
-            </div>
-          </div>
-          <h2 id="accordion-flush-heading-3">
-            <button type="button" class="flex items-center justify-between w-full py-5 font-medium rtl:text-right text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400 gap-3" data-accordion-target="#accordion-flush-body-3" aria-expanded="false" aria-controls="accordion-flush-body-3">
-              <span>What are the differences between Flowbite and Tailwind UI?</span>
-              <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
-              </svg>
-            </button>
-          </h2>
-          <div id="accordion-flush-body-3" class="hidden" aria-labelledby="accordion-flush-heading-3">
-            <div class="py-5 border-b border-gray-200 dark:border-gray-700">
-              <p class="mb-2 text-gray-500 dark:text-gray-400">The main difference is that the core components from Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone components, whereas Tailwind UI offers sections of pages.</p>
-              <p class="mb-2 text-gray-500 dark:text-gray-400">However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no technical reason stopping you from using the best of two worlds.</p>
-              <p class="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
-              <ul class="ps-5 text-gray-500 list-disc dark:text-gray-400">
-                <li><a href="https://flowbite.com/pro/" class="text-blue-600 dark:text-blue-500 hover:underline">Flowbite Pro</a></li>
-                <li><a href="https://tailwindui.com/" rel="nofollow" class="text-blue-600 dark:text-blue-500 hover:underline">Tailwind UI</a></li>
-              </ul>
-            </div>
-          </div> */}
         </div>
 
         ))):(<div>Loading...</div>)}
 
       </div>
-
-      
-
-
-
-
-
     </div>
   );
 }
