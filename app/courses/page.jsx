032ee5a65eed060f0dab43eface6b30fed05868c
Supra707 <supratikde2@gmail.com>
2024-03-19 23:50:01 +0530

    small cahnge

--- app/courses/page.jsx
+++ app/courses/page.jsx
@@ -1,6 +1,6 @@
-'use client'
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import CourseCard from "../components/core/CourseCard";
 const Courses = () => {
   const [courses, setCourses] = useState([]);
@@ -8,39 +8,47 @@ const Courses = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.post('/api/getallcourse');
+        const response = await axios.post("/api/getallcourse");
         setCourses(response.data.courseList);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
     fetchData();
-  }, []); 
+  }, []);
 
   return (
-    <div className="container mx-auto my-8">
-      <h1 className="text-3xl font-semibold mb-6 ml-10 my-4">Earn Your Degree</h1>
+    <div className=" mr-6 my-8">
+      <h1 className="text-3xl font-semibold mb-6 ml-10 my-4">
+        Earn Your Degree
+      </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ml-10 ">
-        {courses.map(course => (
+        {courses.map((course) => (
           <CourseCard key={course.id} course={course} />
         ))}
       </div>
-      <h1 className="text-3xl font-semibold mb-6 ml-10 my-4">Most Popular Certificates</h1>
+      <h1 className="text-3xl font-semibold mb-6 ml-10 my-4">
+        Most Popular Certificates
+      </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ml-10">
-        {courses.map(course => (
+        {courses.map((course) => (
           <CourseCard key={course.id} course={course} />
         ))}
       </div>
-      <h1 className="text-3xl font-semibold mb-6 ml-10 my-4">Recently Viewed Courses and Specializations</h1>
+      <h1 className="text-3xl font-semibold mb-6 ml-10 my-4">
+        Recently Viewed Courses and Specializations
+      </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ml-10">
-        {courses.map(course => (
+        {courses.map((course) => (
           <CourseCard key={course.id} course={course} />
         ))}
       </div>
-      <h1 className="text-3xl font-semibold mb-6 ml-10 my-4">New on Coursera</h1>
+      <h1 className="text-3xl font-semibold mb-6 ml-10 my-4">
+        New on Coursera
+      </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ml-10">
-        {courses.map(course => (
+        {courses.map((course) => (
           <CourseCard key={course.id} course={course} />
         ))}
       </div>
