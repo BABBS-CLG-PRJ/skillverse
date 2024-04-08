c316d29f1680b1b17680a1a4f4f6e39d89efab38
Supratik De <supratikde2@gmail.com>
2024-04-08 15:27:32 +0530

    fix

--- app/components/core/CourseCard.jsx
+++ app/components/core/CourseCard.jsx
@@ -6,7 +6,7 @@ import { Skeleton, SkeletonCircle, SkeletonText, Box } from "@chakra-ui/react";
 const CourseCard = ({ course, setCourseId, loading }) => {
   return (
     <>
-    {!loading && (
+    {loading && (
        <button
         onClick={() => {
           setCourseId(course._id);
@@ -51,7 +51,7 @@ const CourseCard = ({ course, setCourseId, loading }) => {
         </div>
       </button>
     )}
-    {loading && (
+    {!loading && (
       <Box padding="6" boxShadow="lg" bg="white">
       <SkeletonCircle size="10" />
       <SkeletonText mt="4" noOfLines={4} spacing="4" skeletonHeight="2" />
