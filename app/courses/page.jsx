19c402de3d01d79a79d444427238e8b51cb0928b
Supratik De <supratikde2@gmail.com>
2024-04-08 15:42:20 +0530

    fix

--- app/courses/page.jsx
+++ app/courses/page.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import CourseCard from "../components/core/CourseCard";
 import Link from "next/link";
-import { Skeleton, SkeletonCircle, SkeletonText } from '@chakra-ui/react'
+import { Skeleton, SkeletonCircle, SkeletonText, Box } from "@chakra-ui/react";
 
 const Courses = () => {
   const [courses, setCourses] = useState([]);
@@ -22,68 +22,116 @@ const Courses = () => {
 
     fetchData();
   }, []);
-
+  const loader = [1, 2, 3, 4];
   return (
     <div className=" mr-6 my-8">
       <h1 className="text-3xl font-semibold mb-6 ml-10 my-4">
         Earn Your Degree
       </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ml-10 ">
-        {courses.map((course) => (
-          <Link href={`courses/${CourseId}`}>
-            <CourseCard
-              key={course.id}
-              course={course}
-              setCourseId={setCourseId}
-              loading={loading}
-            />
-          </Link>
-        ))}
+      {!loading &&
+          courses.map((course) => (
+            <Link href={`courses/${CourseId}`}>
+              <CourseCard
+                key={course.id}
+                course={course}
+                setCourseId={setCourseId}
+              />
+            </Link>
+          ))}
+        {loading &&
+          loader.map((load) => (
+            <Box padding="6" boxShadow="lg" bg="white">
+              <SkeletonCircle size="10" />
+              <SkeletonText
+                mt="4"
+                noOfLines={4}
+                spacing="4"
+                skeletonHeight="2"
+              />
+            </Box>
+          ))}
       </div>
       <h1 className="text-3xl font-semibold mb-6 ml-10 my-4">
         Most Popular Certificates
       </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ml-10">
-        {courses.map((course) => (
-          <Link href={`courses/${CourseId}`}>
-            <CourseCard
-              key={course.id}
-              course={course}
-              setCourseId={setCourseId}
-              loading={loading}
-            />
-          </Link>
-        ))}
+        {!loading &&
+          courses.map((course) => (
+            <Link href={`courses/${CourseId}`}>
+              <CourseCard
+                key={course.id}
+                course={course}
+                setCourseId={setCourseId}
+              />
+            </Link>
+          ))}
+        {loading &&
+          loader.map((load) => (
+            <Box padding="6" boxShadow="lg" bg="white">
+              <SkeletonCircle size="10" />
+              <SkeletonText
+                mt="4"
+                noOfLines={4}
+                spacing="4"
+                skeletonHeight="2"
+              />
+            </Box>
+          ))}
       </div>
       <h1 className="text-3xl font-semibold mb-6 ml-10 my-4">
         Recently Viewed Courses and Specializations
       </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ml-10">
-        {courses.map((course) => (
-          <Link href={`courses/${CourseId}`}>
-            <CourseCard
-              key={course.id}
-              course={course}
-              setCourseId={setCourseId}
-              loading={loading}
-            />
-          </Link>
-        ))}
+      {!loading &&
+          courses.map((course) => (
+            <Link href={`courses/${CourseId}`}>
+              <CourseCard
+                key={course.id}
+                course={course}
+                setCourseId={setCourseId}
+              />
+            </Link>
+          ))}
+        {loading &&
+          loader.map((load) => (
+            <Box padding="6" boxShadow="lg" bg="white">
+              <SkeletonCircle size="10" />
+              <SkeletonText
+                mt="4"
+                noOfLines={4}
+                spacing="4"
+                skeletonHeight="2"
+              />
+            </Box>
+          ))}
       </div>
       <h1 className="text-3xl font-semibold mb-6 ml-10 my-4">
         New on Coursera
       </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ml-10">
-        {courses.map((course) => (
-          <Link href={`courses/${CourseId}`}>
-            <CourseCard
-              key={course.id}
-              course={course}
-              setCourseId={setCourseId}
-              loading={loading}
-            />
-          </Link>
-        ))}
+      {!loading &&
+          courses.map((course) => (
+            <Link href={`courses/${CourseId}`}>
+              <CourseCard
+                key={course.id}
+                course={course}
+                setCourseId={setCourseId}
+              />
+            </Link>
+          ))}
+        {loading &&
+          loader.map((load) => (
+            <Box padding="6" boxShadow="lg" bg="white">
+              <SkeletonCircle size="10" />
+              <SkeletonText
+                mt="4"
+                noOfLines={4}
+                spacing="4"
+                skeletonHeight="2"
+              />
+            </Box>
+          ))}
       </div>
     </div>
   );
