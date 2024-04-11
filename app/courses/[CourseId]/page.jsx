566cc41c9b2518be01be3eb9844d34cb2acb5cb8
Bineet09 <bineetchattopadhyay00@gmail.com>
2024-04-11 10:54:23 +0530

    total ratings in course details

--- app/courses/[CourseId]/page.jsx
+++ app/courses/[CourseId]/page.jsx
@@ -1,11 +1,12 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import CommentSection from "../../components/common/CommentSection";
-import Loading from './Loading';
+import Loading from "./Loading";
+import RatingStars from "../../components/common/RatingStars";
 import { Tabs, TabList, Tab, TabPanel } from "react-tabs";
 import axios from "axios";
 import CTAButton from "../../components/core/Button";
-import { SquarePlay, Zap, ShoppingCart } from "lucide-react";
+import { SquarePlay, Zap, ShoppingCart, MonitorSmartphone, Trophy, BookOpenText } from "lucide-react";
 import {
   Accordion,
   AccordionItem,
@@ -14,7 +15,19 @@ import {
   AccordionIcon,
   Box,
 } from "@chakra-ui/react";
-import { Card, CardHeader, CardBody, CardFooter, Stack, Heading, Divider, ButtonGroup, Button, Image, Text } from "@chakra-ui/react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Stack,
+  Heading,
+  Divider,
+  ButtonGroup,
+  Button,
+  Image,
+  Text,
+} from "@chakra-ui/react";
 
 const CoursePage = ({ params }) => {
   const [activeIndex, setActiveIndex] = useState(null);
@@ -44,127 +57,156 @@ const CoursePage = ({ params }) => {
     fetchData();
   }, []); // Empty dependency array ensures the effect runs once when the component mounts
 
+  var totalRatings = 0;
+  var i;
+  useEffect(() => {
+    const calculateRatings = async () => {
+      for(i=0; i<courseData?.reviews.length; i++){
+        totalRatings += 1;
+      }
+    };
+
+    calculateRatings();
+  }, [])
+
   return (
     <div>
-      {loading ? (<Loading />):(
-      <div className="grid min-h-[calc(100vh-3.5rem)] place-items-center">
-        <div className="mx-auto flex w-11/12 max-w-maxContent flex-col justify-between gap-y-12 py-12 md:flex-row md:gap-y-0 md:gap-x-12">
-          <div className="mx-auto w-full md:mx-0">
-            <h1 className="text-[1.875rem] font-bold leading-[2.375rem] text-richblack-900">
-              {courseData?.title}
-            </h1>
-            <p className="mt-4 text-[1.125rem] leading-[1.625rem]">
-              <span className="text-richblack-800">
-                {courseData?.description}
-              </span>{" "}
-              {/* <span className="font-edu-sa font-bold italic text-primary-violet">
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="grid min-h-[calc(100vh-3.5rem)] place-items-center">
+          <div className="mx-auto flex w-11/12 max-w-maxContent flex-col justify-between gap-y-12 py-12 md:flex-row md:gap-y-0 md:gap-x-12">
+            <div className="mx-auto w-full md:mx-0">
+              <h1 className="text-[1.875rem] font-bold leading-[2.375rem] text-richblack-900">
+                {courseData?.title}
+              </h1>
+              <p className="mt-4 text-[1.125rem] leading-[1.625rem]">
+                <span className="text-richblack-800">
+                  {courseData?.description}
+                </span>{" "}
+                {/* <span className="font-edu-sa font-bold italic text-primary-violet">
                     {description2}
                   </span> */}
-            </p>
-            <div className="container mx-auto px-4 py-10 ">
-              <div className="grid grid-rows-1 md:grid-cols-1 gap-8">
-                <div className="card bg-white rounded-lg shadow-md p-6">
-                  <h3 className="text-2xl font-medium mb-4">
-                    What you'll learn
-                  </h3>
-                  <ul className="list-disc space-y-2">
-                    <li className="text-base">
-                      Latest technologies: Javascript, React, Node, Web3
-                    </li>
-                    <li className="text-base">Build websites and web apps</li>
-                    <li className="text-base">Freelance web development</li>
-                    <li className="text-base">
-                      Master frontend & backend development
-                    </li>
-                    <li className="text-base">
-                      Professional developer best practices
-                    </li>
-                  </ul>
-                </div>
-                <div className="card bg-white rounded-lg shadow-md p-6">
-                  <h3 className="text-2xl font-medium mb-4">
-                    This course includes
-                  </h3>
-                  <ul className="list-disc space-y-2">
-                    <li className="text-base">61 hours on-demand video</li>
-                    <li className="text-base">194 downloadable resources</li>
-                    <li className="text-base">Mobile and TV access</li>
-                    <li className="text-base">Certificate of completion</li>
-                  </ul>
+              </p>
+              <span class="text-black-800 text-md font-semibold flex-row px-2.5 py-0.5">
+                <p className="text-gray-700 flex gap-5">
+                  <RatingStars Review_Count={courseData.rating} />
+                  {courseData.rating}
+                  ({totalRatings} ratings)
+                </p>
+              </span>
+
+              <span>
+                <p>
+                  Created By 
+                </p>
+              </span>
+
+              <div className=" p-6">
+                    <h3 className="text-2xl font-medium mb-4">
+                      This course includes
+                    </h3>
+                    <ul className="list-none space-y-2">
+                      <li className="text-base flex items-center bg-slate-500"> <span><SquarePlay /> 61 hours on-demand video</span></li>
+                      <li className="text-base"><BookOpenText />194 downloadable resources</li>
+                      <li className="text-base"><MonitorSmartphone />Mobile and TV access</li>
+                      <li className="text-base"><Trophy />Certificate of completion</li>
+                    </ul>
+                  </div>
+
+              <div className="container mx-auto px-4 py-10 ">
+                <div className="grid grid-rows-1 md:grid-cols-1 gap-8">
+
+                  {/* <div className=" p-6">
+                    <h3 className="text-2xl font-medium mb-4">
+                      This course includes
+                    </h3>
+                    <ul className="list-disc space-y-2">
+                      <li className="text-base">61 hours on-demand video</li>
+                      <li className="text-base">194 downloadable resources</li>
+                      <li className="text-base">Mobile and TV access</li>
+                      <li className="text-base">Certificate of completion</li>
+                    </ul>
+                  </div> */}
                 </div>
               </div>
             </div>
-          </div>
-          <div className="relative ml-5 mx-auto w-11/12 max-w-[450px] md:mx-0">
-            <div className="sticky top-5 w-9/12 bg-white text-black shadow-lg rounded-md">
-              <Card maxW="sm" __css={{padding: '8px'}}>
-                <CardBody>
-                  <Image
-                    src={courseData?.imageUrl}
-                    alt="Green double couch with wooden legs"
-                    borderRadius="md"
-                  />
-                  <Stack mt="6" spacing="3">
-                    <Heading size="md">{courseData?.title}</Heading>
-                    <Text color="black" fontSize="2xl">
-                    ₹ {courseData?.price}
-                    </Text>
-                  </Stack>
-                </CardBody>
-                <Divider />
+            <div className="relative ml-5 mx-auto w-11/12 max-w-[450px] md:mx-0">
+              <div className="sticky top-5 w-9/12 bg-white text-black shadow-lg rounded-md">
+                <Card maxW="sm" __css={{ padding: "8px" }}>
+                  <CardBody>
+                    <Image
+                      src={courseData?.imageUrl}
+                      alt="Green double couch with wooden legs"
+                      borderRadius="md"
+                    />
+                    <Stack mt="6" spacing="3">
+                      <Heading size="md">{courseData?.title}</Heading>
+                      <Text color="black" fontSize="2xl">
+                        ₹ {courseData?.price}
+                      </Text>
+                    </Stack>
+                  </CardBody>
+                  <Divider />
                   <div className="flex-col items-center p-2">
-                    <div className="bg-yellow-400 text-center p-2 rounded-md font-bold cursor-pointer flex items-center justify-center gap-2"><Zap/> Buy Now</div>
-                    <div className="text-black mt-2 border-2 rounded-md hover:bg-yellow-400 cursor-pointer border-yellow-400 text-center p-2 font-bold flex items-center justify-center gap-2"><ShoppingCart/>Add to Cart</div>
+                    <div className="bg-yellow-400 text-center p-2 rounded-md font-bold cursor-pointer flex items-center justify-center gap-2">
+                      <Zap /> Buy Now
+                    </div>
+                    <div className="text-black mt-2 border-2 rounded-md hover:bg-yellow-400 cursor-pointer border-yellow-400 text-center p-2 font-bold flex items-center justify-center gap-2">
+                      <ShoppingCart />
+                      Add to Cart
+                    </div>
                   </div>
-                <Divider/>
-                <p className="text-[12px] text-gray-700 text-center border-t-2 py-2">
+                  <Divider />
+                  <p className="text-[12px] text-gray-700 text-center border-t-2 py-2">
                     30 day money back guarantee
                   </p>
-              </Card>
+                </Card>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="mx-auto p-6 lg:w-[60%] w-full">
-          <CommentSection />
-        </div>
 
-        <div className="mx-auto flex w-11/12 max-w-maxContent flex-col-reverse justify-between gap-y-12 py-12 md:flex-row md:gap-y-0 md:gap-x-12">
-          <div className=" mx-auto w-3/5 md:mx-0">
-            <h2 className="text-[1.875rem] font-bold leading-[2.375rem] text-richblack-900">
-              Curriculum
-            </h2>
+          <div className="mx-auto flex w-11/12 max-w-maxContent flex-col-reverse justify-between gap-y-12 py-12 md:flex-row md:gap-y-0 md:gap-x-12">
+            <div className=" mx-auto w-3/5 md:mx-0">
+              <h2 className="text-[1.875rem] font-bold leading-[2.375rem] text-richblack-900">
+                Curriculum
+              </h2>
 
-            <Accordion defaultIndex={[0]} allowMultiple>
-              {courseData ? (
-                courseData.curriculum.map((course, index) => (
-                  <AccordionItem>
-                    <h2>
-                      <AccordionButton
-                        _expanded={{ bg: "gray", color: "white" }}
-                      >
-                        <Box as="span" flex="1" textAlign="left">
-                          {course.sectionTitle}
-                        </Box>
-                        <AccordionIcon />
-                      </AccordionButton>
-                    </h2>
-                    {course.lectures.map((lecture) => (
-                      <AccordionPanel className="w-full" pb={4}>
-                        <div className="pl-4 flex gap-x-1">
-                          <SquarePlay />
-                          {lecture.lectureTitle}
-                        </div>
-                      </AccordionPanel>
-                    ))}
-                  </AccordionItem>
-                ))
-              ) : (
-                <div>Loading...</div>
-              )}
-            </Accordion>
+              <Accordion defaultIndex={[0]} allowMultiple>
+                {courseData ? (
+                  courseData.curriculum.map((course, index) => (
+                    <AccordionItem>
+                      <h2>
+                        <AccordionButton
+                          _expanded={{ bg: "gray", color: "white" }}
+                        >
+                          <Box as="span" flex="1" textAlign="left">
+                            {course.sectionTitle}
+                          </Box>
+                          <AccordionIcon />
+                        </AccordionButton>
+                      </h2>
+                      {course.lectures.map((lecture) => (
+                        <AccordionPanel className="w-full" pb={4}>
+                          <div className="pl-4 flex gap-x-1">
+                            <SquarePlay />
+                            {lecture.lectureTitle}
+                          </div>
+                        </AccordionPanel>
+                      ))}
+                    </AccordionItem>
+                  ))
+                ) : (
+                  <div>Loading...</div>
+                )}
+              </Accordion>
+            </div>
+          </div>
+
+          <div className="mx-auto p-6 lg:w-[60%] w-full">
+            <CommentSection />
           </div>
         </div>
-      </div>
       )}
 
       {/* <div className="h-2/3 flex flex-col md:flex-row justify-between text-brown px-14 mt-5 items-center">
