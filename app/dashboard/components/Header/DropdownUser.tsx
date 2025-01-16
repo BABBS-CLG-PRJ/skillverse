7f03d9f04e055b066e4ff015bfa133a5351a21f7
Supratik De <supratikde2@gmail.com>
2025-01-16 17:28:04 +0530

    completely done

--- app/dashboard/components/Header/DropdownUser.tsx
+++ app/dashboard/components/Header/DropdownUser.tsx
@@ -34,7 +34,10 @@ const DropdownUser = () => {
     document.addEventListener("keydown", keyHandler);
     return () => document.removeEventListener("keydown", keyHandler);
   });
-
+  const handlelogout=()=>{
+    localStorage.removeItem('authtoken');
+    window.location.reload();
+  }
   return (
     <div className="relative">
       <Link
@@ -158,7 +161,7 @@ const DropdownUser = () => {
             </Link>
           </li>
         </ul>
-        <button className="flex items-center gap-3.5 py-4 px-6 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base">
+        <button onClick={handlelogout} className="flex items-center gap-3.5 py-4 px-6 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base">
           <svg
             className="fill-current"
             width="22"
