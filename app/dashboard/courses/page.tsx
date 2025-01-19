72aa506ffbfdf819437279e08e8c7ad075827d5b
Supratik De <supratikde2@gmail.com>
2025-01-20 00:57:23 +0530

    ai quiz from updated no more dropdowns

--- app/dashboard/courses/page.tsx
+++ app/dashboard/courses/page.tsx
@@ -9,79 +9,28 @@ import {
   ModalOverlay,
   ModalContent,
   ModalHeader,
-  ModalFooter,
-  ModalBody,
   ModalCloseButton,
   useDisclosure,
   Button,
 } from "@chakra-ui/react";
 import QuizForm from "../components/common/quizform";
 import Aiform from "../components/common/aiform";
+
 const Courses = ({ user }) => {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-
-  const toggleDropdown = () => {
-    setIsDropdownOpen((prev) => !prev);
-  };
-  const [isOpen, setIsOpen] = useState(false);
-  const [modalContent, setModalContent] = useState("AI"); // Default content is "AI"
-
-  const onOpen = (content) => {
-    setModalContent(content);
-    setIsOpen(true);
-  };
-
-  const onClose = () => {
-    setIsOpen(false);
-  };
-
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <div className="flex flex-col gap-4 md:gap-6 2xl:gap-7.5">
       {/* Create Quiz Button */}
       <div className="flex justify-center text-center order-first">
         <div className="relative inline-block text-center w-full md:w-125">
           <Button
-            onClick={toggleDropdown}
+            onClick={onOpen}
             className="w-full"
             colorScheme={"yellow"}
             size="lg"
           >
-            Create Quiz for your Courses
+           
             <svg
-              className="ml-2 w-5 h-5"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                fillRule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 011.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </Button>
-
-          {/* Dropdown Menu */}
-          <div
-            className={`absolute right-0 mt-2 w-full bg-white border border-gray-200 rounded-md shadow-lg origin-top-right transition-all duration-200 ease-out transform dark:bg-yellow-100 dark:border-yellow-800 ${
-              isDropdownOpen
-                ? "scale-100 opacity-100"
-                : "scale-95 opacity-0 pointer-events-none"
-            }`}
-          >
-            <div
-              className="py-1 hover:bg-violet-200 bg-violet-100"
-              role="menu"
-              aria-orientation="vertical"
-              aria-labelledby="dropdownButton"
-            >
-              {/* Generate quiz with AI */}
-              <button
-                onClick={() => onOpen("AI")}
-                className="w-full justify-center px-4 py-2 text-lg transition-all duration-500 font-bold text-gray-700 dark:text-black hover:bg-violet-200 bg-violet-100 flex items-center"
-              >
-                <svg
                   className="w-5 h-5 mr-2"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
@@ -106,24 +55,10 @@ const Courses = ({ user }) => {
                     </radialGradient>
                   </defs>
                 </svg>
-                Generate with AI
-              </button>
-              {/* Create Quiz Manually */}
-              <button
-                onClick={() => onOpen("Manual")}
-                className="w-full justify-center px-4 py-2 text-lg transition-all duration-500 font-bold text-gray-700 hover:bg-violet-200 bg-violet-100 flex items-center"
-              >
-                <Image
-                  src={gear}
-                  alt="gear icon"
-                  width={30}
-                  height={30}
-                  className="mr-2"
-                />
-                <p>Create Manually</p>
-              </button>
-            </div>
-          </div>
+                Create Quiz for your Courses
+          </Button>
+
+          
         </div>
       </div>
 
@@ -139,17 +74,13 @@ const Courses = ({ user }) => {
         <ModalContent>
           <ModalHeader>
             <h1 className="text-3xl font-bold text-gray-800 border-b border-amber-200 pb-2 transition-colors duration-300">
-              {modalContent === "AI"
-                ? "Generate Quiz with AI"
-                : "Create Quiz Manually"}
+            Generate Quiz with AI or Manually
             </h1>
           </ModalHeader>
           <ModalCloseButton />
-          {modalContent === "AI" ? (
-            <Aiform user={user}/>
-          ) : (
+          
             <QuizForm user={user}/>
-          )}
+          
         </ModalContent>
       </Modal>
 
