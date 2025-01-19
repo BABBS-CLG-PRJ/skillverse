9f1d6b366ea8022e77434d1a209200dbaff5b864
Arnab Nandi <arnabnandi7074@gmail.com>
2025-01-19 19:00:22 +0530

    final

--- app/api/getotp/route.js
+++ app/api/getotp/route.js
@@ -17,7 +17,6 @@ const transporter = nodemailer.createTransport({
 
 export async function POST(req, res) {
     const { email } = await req.json();
-    console.log(email);
 
     try {
         // Generate the otp
