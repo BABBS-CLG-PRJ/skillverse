f55c87f9aec3ddde5fe8050ae42402520eb45391
Arnab Nandi <arnabnandi7074@gmail.com>
2025-01-21 09:38:50 +0530

    course card already enrolled fixed & quizcard fixed

--- app/courses/page.jsx
+++ app/courses/page.jsx
@@ -4,17 +4,24 @@ import axios from "axios";
 import CourseCard from "../components/core/CourseCard";
 import Link from "next/link";
 import { Skeleton } from "@chakra-ui/react";
+import { useCookies } from "next-client-cookies";
 
 const Courses = () => {
   const [courses, setCourses] = useState([]);
   const [loading, setLoading] = useState(true);
   const [CourseId, setCourseId] = useState("");
+  const [courseEnrolled, setCourseEnrolled] = useState([]);
+  const cookieStore = useCookies();
+  const authToken = cookieStore.get('authtoken');
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.post("/api/getallcourse");
+        const userDetails = await axios.post("/api/verifytoken", {token: authToken});
+        const courseIds = userDetails.data.decodedToken.profileObject.coursesEnrolled.map(entry => entry.course);
         setCourses(response.data.courseList || []); // Ensure courses is an array
+        setCourseEnrolled(courseIds);
         setLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -56,7 +63,7 @@ const Courses = () => {
 
     return courses.map((course) => (
       <Link key={course.id} href={`courses/${course.id}`} prefetch={true}>
-        <CourseCard course={course} setCourseId={setCourseId} />
+        <CourseCard course={course} courseEnrolled={courseEnrolled} setCourseId={setCourseId} />
       </Link>
     ));
   };
